cb2e65a2eb6d4a53013b9de8d351b4809818ae8a
paulmhrdka <pmsp3108@gmail.com>
2024-04-03 16:32:34 +0700

    FIX: change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 const host = process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0';
 
 app.get('/', (req, res) => {
